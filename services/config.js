474a67aad0d1fb3196fc0b4cebd31e57fd3f2eda
ildi <ildi@clinical.ly>
2021-11-27 18:54:08 -0500

    test git ignore

--- services/config.js
+++ services/config.js
@@ -1,7 +1,7 @@
 export default {
   aws: {
     region: "us-east-1",
-    accessKeyId: "put access key id here",
-    secretAccessKey: "put secret access key here" ,
+    accessKeyId: "paste access key id here",
+    secretAccessKey: "past",
   },
 };
