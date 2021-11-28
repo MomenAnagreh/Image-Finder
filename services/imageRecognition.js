5856ce35eda3674f666be81697af219d268818fd
ildi <ildi@clinical.ly>
2021-11-27 19:26:44 -0500

    set up AWS image recognition preliminary

---
+++ services/imageRecognition.js
@@ -0,0 +1,23 @@
+import {
+  RekognitionClient,
+  DetectLabelsCommand,
+} from "@aws-sdk/client-rekognition";
+import config from "./config.js";
+
+const {
+  aws: { accessKeyId, secretAccessKey, region },
+} = config;
+
+const client = new RekognitionClient({
+  region,
+  credentials: { accessKeyId, secretAccessKey },
+});
+
+export const getLabelsForImage = async (b64Image) => {
+  let imageBytes = Buffer.from(b64Image, "base64");
+
+  const command = new DetectLabelsCommand({ Image: { Bytes: imageBytes } });
+  const response = await client.send(command);
+
+  return response;
+};
--- services/index.js
+++ services/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { getLabelsForImage } from "./imageRecognition.js";
 
 const port = process.env.PORT || 9000;
 
 const app = express();
+
+app.post("/identify", (req, res) => {
+  const labels = getLabelsForImage(req.body);
+  res.send(labels);
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
